636a4700a28626a0f408a79eb58b79f242e47e6c
AndiezSatria <andis.permana12@gmail.com>
2021-12-28 19:24:26 +0700

    Add Home Page

---
+++ app/src/main/java/com/andiez/moviecatalogueadvance/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.andiez.moviecatalogueadvance.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andiez.moviecatalogueadvance.R
+
+class HomeFragment : Fragment(R.id) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
