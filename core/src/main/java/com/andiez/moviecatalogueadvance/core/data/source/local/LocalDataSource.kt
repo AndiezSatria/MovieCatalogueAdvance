206c042ad5b26fa73907977946b2a7b819eb2e9f
AndiezSatria <andis.permana12@gmail.com>
2021-12-29 12:36:30 +0700

    Create Data Sources for local and remote data

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
@@ -1,4 +1,12 @@
 package com.andiez.moviecatalogueadvance.core.data.source.local
 
-class LocalDataSource {
+import com.andiez.moviecatalogueadvance.core.data.source.local.entity.GenreEntity
+import com.andiez.moviecatalogueadvance.core.data.source.local.realm.IMovieDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LocalDataSource @Inject constructor(private val movieDao: IMovieDao) {
+    fun getGenre(): Flow<List<GenreEntity>> = movieDao.getGenres()
 }
--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/RemoteDataSource.kt
@@ -1,4 +1,30 @@
 package com.andiez.moviecatalogueadvance.core.data.source.remote
 
-class RemoteDataSource {
+import android.util.Log
+import com.andiez.moviecatalogueadvance.core.data.source.remote.network.ApiResponse
+import com.andiez.moviecatalogueadvance.core.data.source.remote.network.ApiService
+import com.andiez.moviecatalogueadvance.core.data.source.remote.response.MovieResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
+    suspend fun getMovies(): Flow<ApiResponse<List<MovieResponse>>> = flow {
+        try {
+            val response = apiService.getMovie()
+            val dataArray = response.results
+            if (dataArray.isNotEmpty()) {
+                emit(ApiResponse.Success(response.results))
+            } else {
+                emit(ApiResponse.Empty)
+            }
+        } catch (e: Exception) {
+            emit(ApiResponse.Error(e.toString()))
+            Log.e("RemoteDataSource", e.toString())
+        }
+    }.flowOn(Dispatchers.IO)
 }
