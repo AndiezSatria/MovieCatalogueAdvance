77f83ffcba14499942e351e8efd6e7252ebc044b
AndiezSatria <andis.permana12@gmail.com>
2022-01-01 19:12:36 +0700

    Change layout to bottom navigation bar

--- app/src/main/java/com/andiez/moviecatalogueadvance/MainActivity.kt
+++ app/src/main/java/com/andiez/moviecatalogueadvance/MainActivity.kt
@@ -2,10 +2,25 @@ package com.andiez.moviecatalogueadvance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.andiez.moviecatalogueadvance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        val toolbar = binding.appbarMain.toolbar
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        NavigationUI.setupWithNavController(toolbar, navController)
+        setSupportActionBar(toolbar)
     }
 }
