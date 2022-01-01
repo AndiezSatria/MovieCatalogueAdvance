af57fca64bc0a80271222881162ad2488dc44e77
AndiezSatria <andis.permana12@gmail.com>
2022-01-01 12:33:12 +0700

    Create domain data class for movie

---
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/domain/model/Movie.kt
@@ -0,0 +1,10 @@
+package com.andiez.moviecatalogueadvance.core.domain.model
+
+data class Movie(
+    var id: Int = 0,
+    var originalTitle: String = "",
+    var title: String = "",
+    var img: String = "",
+    var releaseDate: String = "",
+    var voteAverage: Double = 0.0,
+)
