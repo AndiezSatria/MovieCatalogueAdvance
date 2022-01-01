f1224cddbb063baa18a1f5268a0f6ec9193706dd
AndiezSatria <andis.permana12@gmail.com>
2022-01-01 12:32:52 +0700

    Add insert and get cache for movie entity

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.andiez.moviecatalogueadvance.core.data.source.local
 
 import com.andiez.moviecatalogueadvance.core.data.source.local.entity.GenreEntity
+import com.andiez.moviecatalogueadvance.core.data.source.local.entity.MovieEntity
 import com.andiez.moviecatalogueadvance.core.data.source.local.realm.IMovieDao
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,4 +10,9 @@ import javax.inject.Singleton
 @Singleton
 class LocalDataSource @Inject constructor(private val movieDao: IMovieDao) {
     fun getGenre(): Flow<List<GenreEntity>> = movieDao.getGenres()
+    fun getMovies(): Flow<List<MovieEntity>> = movieDao.getMovies()
+    fun getPopularMovies(): Flow<List<MovieEntity>> = movieDao.getPopularMovies()
+
+    suspend fun insertGenres(genres: List<GenreEntity>) = movieDao.insertGenres(genres)
+    suspend fun insertMovies(movies: List<MovieEntity>) = movieDao.insertMovies(movies)
 }
--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/realm/IMovieDao.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/realm/IMovieDao.kt
@@ -1,8 +1,15 @@
 package com.andiez.moviecatalogueadvance.core.data.source.local.realm
 
 import com.andiez.moviecatalogueadvance.core.data.source.local.entity.GenreEntity
+import com.andiez.moviecatalogueadvance.core.data.source.local.entity.MovieEntity
 import kotlinx.coroutines.flow.Flow
 
 interface IMovieDao {
     fun getGenres(): Flow<List<GenreEntity>>
+    suspend fun insertGenres(genres: List<GenreEntity>)
+
+    fun getMovies(): Flow<List<MovieEntity>>
+    suspend fun insertMovies(movies: List<MovieEntity>)
+
+    fun getPopularMovies(): Flow<List<MovieEntity>>
 }
