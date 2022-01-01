c91baa49684d884e4d24e56f665b4b111e75c878
AndiezSatria <andis.permana12@gmail.com>
2022-01-01 19:13:28 +0700

    Remove inject for realm configuration

--- app/src/main/java/com/andiez/moviecatalogueadvance/BaseApplication.kt
+++ app/src/main/java/com/andiez/moviecatalogueadvance/BaseApplication.kt
@@ -4,15 +4,15 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import io.realm.Realm
 import io.realm.RealmConfiguration
-import javax.inject.Inject
 
 @HiltAndroidApp
 class BaseApplication : Application() {
-    @Inject
-    lateinit var config: RealmConfiguration
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        Realm.setDefaultConfiguration(config)
+        val configuration = RealmConfiguration.Builder()
+            .schemaVersion(1L)
+            .build()
+        Realm.setDefaultConfiguration(configuration)
     }
 }
