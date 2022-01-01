a7f57ae206d4b3fdb0a4ac0b26d5ffe7b7ee50f1
AndiezSatria <andis.permana12@gmail.com>
2022-01-01 19:13:59 +0700

    Setting home page

--- app/src/main/java/com/andiez/moviecatalogueadvance/home/HomeFragment.kt
+++ app/src/main/java/com/andiez/moviecatalogueadvance/home/HomeFragment.kt
@@ -2,13 +2,16 @@ package com.andiez.moviecatalogueadvance.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import com.andiez.moviecatalogueadvance.MainActivity
 import com.andiez.moviecatalogueadvance.R
+import com.andiez.moviecatalogueadvance.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+        (requireActivity() as MainActivity).supportActionBar?.setTitle(R.string.text_home)
     }
 }
