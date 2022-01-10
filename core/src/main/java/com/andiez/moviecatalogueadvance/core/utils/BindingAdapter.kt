fade8a9777ae814df148c8fbe2828f89726f3691
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:37:11 +0700

    Add Binding Adapter to show image

---
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/utils/BindingAdapter.kt
@@ -0,0 +1,25 @@
+package com.andiez.moviecatalogueadvance.core.utils
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.andiez.moviecatalogueadvance.core.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("bindPoster")
+fun bindPoster(imageView: ImageView, posterPath: String?) {
+    posterPath?.let {
+        val url = "https://image.tmdb.org/t/p/w185$it"
+        val imgUri = url.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                    .override(150, 200)
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )
+            .into(imageView)
+    }
+}
