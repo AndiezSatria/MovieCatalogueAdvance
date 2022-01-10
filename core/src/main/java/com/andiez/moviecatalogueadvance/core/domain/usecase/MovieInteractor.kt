2e82d6bcc0f875d83db15f654e02a2b450761d98
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:33:17 +0700

    Add function for Tv Show insertion and get

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/domain/usecase/MovieInteractor.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/domain/usecase/MovieInteractor.kt
@@ -2,6 +2,7 @@ package com.andiez.moviecatalogueadvance.core.domain.usecase
 
 import com.andiez.moviecatalogueadvance.core.data.Resource
 import com.andiez.moviecatalogueadvance.core.domain.model.Movie
+import com.andiez.moviecatalogueadvance.core.domain.model.TvShow
 import com.andiez.moviecatalogueadvance.core.domain.repository.IMovieRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,6 +10,10 @@ import javax.inject.Inject
 class MovieInteractor @Inject constructor(private val repository: IMovieRepository) : MovieUseCase {
     override fun getMovies(): Flow<Resource<List<Movie>>> {
         return repository.getMovies()
+    }
+
+    override fun getTvShows(): Flow<Resource<List<TvShow>>> {
+        return repository.getTvShows()
     }
 
     override fun getPopularMovies(): Flow<Resource<List<Movie>>> {
--- core/src/main/java/com/andiez/moviecatalogueadvance/core/domain/usecase/MovieUseCase.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/domain/usecase/MovieUseCase.kt
@@ -2,9 +2,11 @@ package com.andiez.moviecatalogueadvance.core.domain.usecase
 
 import com.andiez.moviecatalogueadvance.core.data.Resource
 import com.andiez.moviecatalogueadvance.core.domain.model.Movie
+import com.andiez.moviecatalogueadvance.core.domain.model.TvShow
 import kotlinx.coroutines.flow.Flow
 
 interface MovieUseCase {
     fun getMovies(): Flow<Resource<List<Movie>>>
+    fun getTvShows(): Flow<Resource<List<TvShow>>>
     fun getPopularMovies(): Flow<Resource<List<Movie>>>
 }
