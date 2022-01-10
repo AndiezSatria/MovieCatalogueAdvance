6043c7b375d560417717397318010b7fd3b794fd
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:31:19 +0700

    Add data class for Tv Show (Remote, Local, Domain)

---
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/response/ListTvResponse.kt
@@ -0,0 +1,8 @@
+package com.andiez.moviecatalogueadvance.core.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ListTvResponse(
+    @field:SerializedName("results")
+    var results: List<TvShowResponse>
+)
