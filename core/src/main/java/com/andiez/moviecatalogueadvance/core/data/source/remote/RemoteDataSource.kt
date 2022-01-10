cfae90fd79d21c4233bd7650fa66866fa5462a3b
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:29:59 +0700

    Add function get for tv shows

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/RemoteDataSource.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.andiez.moviecatalogueadvance.core.data.source.remote.network.ApiResponse
 import com.andiez.moviecatalogueadvance.core.data.source.remote.network.ApiService
 import com.andiez.moviecatalogueadvance.core.data.source.remote.response.MovieResponse
+import com.andiez.moviecatalogueadvance.core.data.source.remote.response.TvShowResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -31,6 +32,21 @@ class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
     suspend fun getPopularMovies(): Flow<ApiResponse<List<MovieResponse>>> = flow {
         try {
             val response = apiService.getPopularMovie()
+            val dataArray = response.results
+            if (dataArray.isNotEmpty()) {
+                emit(ApiResponse.Success(response.results))
+            } else {
+                emit(ApiResponse.Empty)
+            }
+        } catch (e: Exception) {
+            emit(ApiResponse.Error(e.toString()))
+            Log.e("RemoteDataSource", e.toString())
+        }
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun getTvShows(): Flow<ApiResponse<List<TvShowResponse>>> = flow {
+        try {
+            val response = apiService.getTvShow()
             val dataArray = response.results
             if (dataArray.isNotEmpty()) {
                 emit(ApiResponse.Success(response.results))
