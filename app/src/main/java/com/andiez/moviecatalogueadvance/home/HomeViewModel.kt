61ddfa3fcc9151e3b19c8c75deb8301d32629fa0
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:36:21 +0700

    Add live data for Tv Shows

---
+++ app/src/main/java/com/andiez/moviecatalogueadvance/home/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.andiez.moviecatalogueadvance.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.andiez.moviecatalogueadvance.core.data.Resource
+import com.andiez.moviecatalogueadvance.core.domain.model.Movie
+import com.andiez.moviecatalogueadvance.core.domain.model.TvShow
+import com.andiez.moviecatalogueadvance.core.domain.usecase.MovieUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val useCase: MovieUseCase) : ViewModel() {
+    fun getPopularMovies(): LiveData<Resource<List<Movie>>> =
+        useCase.getPopularMovies().asLiveData()
+
+    fun getMovies(): LiveData<Resource<List<Movie>>> = useCase.getMovies().asLiveData()
+    fun getTvShows(): LiveData<Resource<List<TvShow>>> = useCase.getTvShows().asLiveData()
+}
