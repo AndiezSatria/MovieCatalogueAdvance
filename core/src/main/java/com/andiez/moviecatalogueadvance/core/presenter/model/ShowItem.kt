cce3ae3948a40ea8253ce4aff8bbf49208939fb1
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:34:17 +0700

    Add presenter model for data class show

---
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/presenter/model/ShowItem.kt
@@ -0,0 +1,8 @@
+package com.andiez.moviecatalogueadvance.core.presenter.model
+
+data class ShowItem(
+    var id: Int = 0,
+    var title: String = "",
+    var img: String = "",
+    var voteAverage: Double = 0.0,
+)
