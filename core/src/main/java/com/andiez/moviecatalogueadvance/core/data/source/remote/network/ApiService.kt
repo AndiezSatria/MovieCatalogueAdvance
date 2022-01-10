a5cf8ba234a7d547d27624ffe134f74f71be58c8
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:28:29 +0700

    Get tv show from API

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/remote/network/ApiService.kt
@@ -1,19 +1,26 @@
 package com.andiez.moviecatalogueadvance.core.data.source.remote.network
 
 import com.andiez.moviecatalogueadvance.core.data.source.remote.response.ListMovieResponse
+import com.andiez.moviecatalogueadvance.core.data.source.remote.response.ListTvResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/movie/now_playing")
+    @GET("movie/now_playing")
     suspend fun getMovie(
         @Query("api_key") apiKey: String = "",
         @Query("page") page: Int = 1
     ): ListMovieResponse
 
-    @GET("/movie/popular")
+    @GET("movie/popular")
     suspend fun getPopularMovie(
         @Query("api_key") apiKey: String = "",
         @Query("page") page: Int = 1
     ): ListMovieResponse
+
+    @GET("tv/airing_today")
+    suspend fun getTvShow(
+        @Query("api_key") apiKey: String = "",
+        @Query("page") page: Int = 1
+    ): ListTvResponse
 }
