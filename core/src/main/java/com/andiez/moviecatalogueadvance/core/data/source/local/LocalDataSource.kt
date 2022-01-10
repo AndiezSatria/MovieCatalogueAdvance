4cabe7a8b3167aa675ec0f7edddc3a7e493c24f4
AndiezSatria <andis.permana12@gmail.com>
2022-01-10 10:32:51 +0700

    Add function for Tv Show insertion and get from database

--- core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/com/andiez/moviecatalogueadvance/core/data/source/local/LocalDataSource.kt
@@ -2,6 +2,7 @@ package com.andiez.moviecatalogueadvance.core.data.source.local
 
 import com.andiez.moviecatalogueadvance.core.data.source.local.entity.GenreEntity
 import com.andiez.moviecatalogueadvance.core.data.source.local.entity.MovieEntity
+import com.andiez.moviecatalogueadvance.core.data.source.local.entity.TvShowEntity
 import com.andiez.moviecatalogueadvance.core.data.source.local.realm.IMovieDao
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,8 +12,10 @@ import javax.inject.Singleton
 class LocalDataSource @Inject constructor(private val movieDao: IMovieDao) {
     fun getGenre(): Flow<List<GenreEntity>> = movieDao.getGenres()
     fun getMovies(): Flow<List<MovieEntity>> = movieDao.getMovies()
+    fun getTvShows(): Flow<List<TvShowEntity>> = movieDao.getTvShows()
     fun getPopularMovies(): Flow<List<MovieEntity>> = movieDao.getPopularMovies()
 
     suspend fun insertGenres(genres: List<GenreEntity>) = movieDao.insertGenres(genres)
+    suspend fun insertTvShows(tvShows: List<TvShowEntity>) = movieDao.insertTvShows(tvShows)
     suspend fun insertMovies(movies: List<MovieEntity>) = movieDao.insertMovies(movies)
 }
